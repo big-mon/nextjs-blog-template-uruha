0f459b6580befdd3ae6d972001c52232e455096b
MON <mrmon93+github@gmail.com>
2025-02-13 00:42:42 +0900

    chore: rename

--- src/app/_components/GlobalHeader.tsx
+++ src/app/_components/GlobalHeader.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Logo } from "@components/logo";
+import { Logo } from "@components/Logo";
 import Menu from "@components/menu";
 
 export const GlobalHeader = () => {
