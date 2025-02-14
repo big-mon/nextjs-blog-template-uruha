7e52122c3406478b24e3c3518284544888cedc8e
MON <mrmon93+github@gmail.com>
2025-02-15 01:48:42 +0900

    fix: タグの比較方法を修正

--- src/app/tag/[tag]/[page]/page.tsx
+++ src/app/tag/[tag]/[page]/page.tsx
@@ -14,7 +14,9 @@ type Params = {
 export default async function Page(props: Params) {
   const tag = decodeURIComponent((await props.params).tag);
   const currentPage = (await props.params).page;
-  const allPosts = getAllPosts().filter((post) => post.tags.includes(tag));
+  const allPosts = getAllPosts().filter((post) =>
+    post.tags.map((t) => t.toLowerCase()).includes(tag.toLowerCase()),
+  );
   const pagePosts = allPosts.slice(
     (currentPage - 1) * PER_PAGE,
     currentPage * PER_PAGE,
